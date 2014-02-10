77cd0db61b97a5128285010def9324416817de9a
Ariel Coppes <ariel.coppes@gmail.com>
2014-02-10 12:53:32 -0200

    initial import

---
+++ Assets/GameScripts/GetComponentBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetComponentBehaviour : MonoBehaviour
+{
+	int counter;
+	
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		for (int i = 0; i < 5000; i++) {
+			MyGetComponent(i);
+		}
+	}
+	
+	void MyGetComponent(int i)
+	{
+		AnotherBehaviour component = this.GetComponent<AnotherBehaviour>();
+		counter++;
+	}
+}
+
