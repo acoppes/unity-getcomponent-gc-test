3156ee1e18d4a3cfbb254228841b42672143c4dc
Ariel Coppes <ariel.coppes@gmail.com>
2014-02-10 14:15:27 -0200

    removed code from script

--- Assets/GameScripts/GetComponentBehaviour.cs
+++ Assets/GameScripts/GetComponentBehaviour.cs
@@ -3,25 +3,11 @@ using System.Collections;
 
 public class GetComponentBehaviour : MonoBehaviour
 {
-	int counter;
-	
-	// Use this for initialization
-	void Start()
-	{
-		
-	}
-	
 	void Update()
 	{
 		for (int i = 0; i < 5000; i++) {
-			MyGetComponent(i);
+			AnotherBehaviour component = this.GetComponent<AnotherBehaviour>();
 		}
-	}
-	
-	void MyGetComponent(int i)
-	{
-		AnotherBehaviour component = this.GetComponent<AnotherBehaviour>();
-		counter++;
 	}
 }
 
